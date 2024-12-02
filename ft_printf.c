692c3c90aa103089f6cb4a49e6cc57d8a05011a3
Salaheddine Sallami <ssallami@c1r4p7.1337.ma>
2024-12-02 12:15:01 +0100

    25 line

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: ssallami <ssallami@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/01 14:28:57 by ssallami          #+#    #+#             */
-/*   Updated: 2024/12/01 14:40:18 by ssallami         ###   ########.fr       */
+/*   Updated: 2024/12/02 12:09:17 by ssallami         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,9 @@ int	ft_printf(const char *foramt, ...)
 	int		j;
 
 	va_start(list, foramt);
-	i = 0;
+	i = -1;
 	j = 0;
-	while (foramt[i] != '\0')
+	while (foramt[++i] != '\0')
 	{
 		if (foramt[i] == '%')
 		{
@@ -31,10 +31,11 @@ int	ft_printf(const char *foramt, ...)
 				foramt[i + 1] == 'u' || foramt[i + 1] == 'x' || 
 				foramt[i + 1] == '%')
 				j = j + ft_conversions(foramt[i++ + 1], list) - 2;
+			else
+				j--;
 		}
 		else
 			write(1, &foramt[i], 1);
-		i++;
 	}
 	va_end(list);
 	return (i + j);
